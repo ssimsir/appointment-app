31a9fc100c5d638dc935bbf1ea95c351bbe1141d
ssimsir <s_simsir@yahoo.com>
2024-04-01 03:51:54 +0300

    App updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Header from "./components/header/Header";
 import Doctors from "./components/main/Doctors";
-import ReservationForm from "./components/main/ReservationForm";
 
 function App() {
   return (
